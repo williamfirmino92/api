febd367ea366f7288d67821a92f005628cf0ef99
William Firmino <williams.an@hotmail.com>
2024-12-02 16:27:35 -0300

    Atualização com spring security com usuário modelo

--- src/main/java/br/com/meteora/api/security/SecurityConfig.java
+++ src/main/java/br/com/meteora/api/security/SecurityConfig.java
@@ -2,30 +2,28 @@ package br.com.meteora.api.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
+@EnableWebSecurity
 public class SecurityConfig {
     
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
-        
-        http
-        .authorizeHttpRequests(auth -> auth
-        .requestMatchers("/index", "/sobre", "/contato").permitAll()
-        .anyRequest().authenticated())
+    public UserDetailsService dadosUsuariosCadastrados() {
+        UserDetails usuario1 = User.builder()
+        .username("joao@email.com")
+        .password("{noop}joao123")
+        .build();
 
-        .formLogin(form -> form
-        .loginPage("/login")
-        .defaultSuccessUrl("/", true)
-        .failureUrl("/login-error")
-        .permitAll())
-        
-        .logout(logout -> logout
-        .logoutSuccessUrl("/"));
-        
-        
-        return http.build();
+        UserDetails usuario2 = User.builder()
+        .username("maria@email.com")
+        .password("{noop}maria123")
+        .build();
+
+        return new InMemoryUserDetailsManager(usuario1, usuario2);
     }
 }
